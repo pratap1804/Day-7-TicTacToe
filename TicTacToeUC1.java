c48f50f324e06f7897526baa5fa1203e9e46b55e
pratap1804 <pratap.1804@gmail.com>
2021-06-28 11:18:26 +0530

    Added: Design of the tictactoe gameboard

---
+++ TicTacToeUC1.java
@@ -0,0 +1,12 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the TicTacToe simulation");
+
+
+        char[][] gameBoard= {{' ','|',' ','|',' '},
+                {'-','+','-','+','-'},
+                {' ','|',' ','|',' '},
+                {'-','+','-','+','-'},
+                {' ','|',' ','|',' '}};
+	}
+}
